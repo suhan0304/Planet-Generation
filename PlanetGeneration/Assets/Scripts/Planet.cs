794a9baefec8ae895f97a5ab9398ce0ec1df2ca6
ZerrY <suhan0304@gmail.com>
2024-02-23 04:57:03 +0900

    Update Planet.cs

--- PlanetGeneration/Assets/Scripts/Planet.cs
+++ PlanetGeneration/Assets/Scripts/Planet.cs
@@ -18,19 +18,25 @@ public class Planet : MonoBehaviour
 
     void Initialized()
     {
-        meshFilters = new MeshFilter[6]; //정육면체이므로 6개
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6]; //정육면체이므로 6개
+        }
         terrainFaces = new TerrainFace[6];
 
         Vector3[] directions = {Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back};
 
         for (int i = 0; i < 6; i++)
         {
-            GameObject meshObj = new GameObject("mesh");
-            meshObj.transform.parent = transform;
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
 
-            meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
-            meshFilters[i] = meshObj.AddComponent<MeshFilter>();
-            meshFilters[i].sharedMesh = new Mesh();
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
 
             terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
         }
