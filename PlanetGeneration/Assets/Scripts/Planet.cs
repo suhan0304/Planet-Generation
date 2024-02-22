394a6f9d3463c290f2025b53aa96ed1d0fe3a801
ZerrY <suhan0304@gmail.com>
2024-02-23 04:54:10 +0900

    Update Planet.cs

--- PlanetGeneration/Assets/Scripts/Planet.cs
+++ PlanetGeneration/Assets/Scripts/Planet.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
+    [Range(2, 25)]
+    public int resolution = 10;
+
     MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+
 
     void Initialized()
     {
         meshFilters = new MeshFilter[6]; //정육면체이므로 6개
+        terrainFaces = new TerrainFace[6]; 
 
         for (int i = 0; i < 6; i++)
         {
             GameObject meshObj = new GameObject("mesh");
             meshObj.transform.parent = transform;
 
-            meshObj.AddComponent<MeshRenderer>();
+            meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
             meshFilters[i] = meshObj.AddComponent<MeshFilter>();
             meshFilters[i].sharedMesh = new Mesh();
+
+            terrainFaces = new TerrainFace(meshFilters[i].sharedMesh, )
         }
     }
 }
