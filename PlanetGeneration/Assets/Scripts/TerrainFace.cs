f1e10f10482535a9d42471ccefb19edd7140f84f
ZerrY <suhan0304@gmail.com>
2024-02-23 04:36:29 +0900

    Update TerrainFace.cs

--- PlanetGeneration/Assets/Scripts/TerrainFace.cs
+++ PlanetGeneration/Assets/Scripts/TerrainFace.cs
@@ -24,5 +24,16 @@ public class TerrainFace
     {
         Vector3[] vertices = new Vector3[resolution * resolution];
         int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
+
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                int i = x + y * resolution;
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+                Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+                vertices[i] = pointOnUnitCube;
+            }
+        }
     }
 }
