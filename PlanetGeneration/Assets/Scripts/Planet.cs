cebbd4e60581f19cdcc3a93a030989a95ab1cced
ZerrY <suhan0304@gmail.com>
2024-02-23 04:52:47 +0900

    Update Planet.cs

--- PlanetGeneration/Assets/Scripts/Planet.cs
+++ PlanetGeneration/Assets/Scripts/Planet.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    MeshFilter[] meshFilters;
 
-    // Update is called once per frame
-    void Update()
+    void Initialized()
     {
-        
+        meshFilters = new MeshFilter[6]; //정육면체이므로 6개
+
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject meshObj = new GameObject("mesh");
+            meshObj.transform.parent = transform;
+
+            meshObj.AddComponent<MeshRenderer>();
+            meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+            meshFilters[i].sharedMesh = new Mesh();
+        }
     }
 }
