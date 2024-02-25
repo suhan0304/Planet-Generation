431d8a428c5e9bb25cefcee648236b4699cd6388
ZerrY <suhan0304@gmail.com>
2024-02-26 04:43:41 +0900

    Update ColorSettings.cs

--- PlanetGeneration/Assets/Scripts/ColorSettings.cs
+++ PlanetGeneration/Assets/Scripts/ColorSettings.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorSettings : MonoBehaviour
+[CreateAssetMenu()]
+public class ColorSettings : ScriptableObject
 {
     // Start is called before the first frame update
     void Start()
