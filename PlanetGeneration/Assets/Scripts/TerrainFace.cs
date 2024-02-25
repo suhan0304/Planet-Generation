25aca8125093f652999e45145b34261d01f81aa1
ZerrY <suhan0304@gmail.com>
2024-02-26 04:54:33 +0900

    Update TerrainFace.cs

--- PlanetGeneration/Assets/Scripts/TerrainFace.cs
+++ PlanetGeneration/Assets/Scripts/TerrainFace.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TerrainFace {
 
-    ShapeGenerator ShapeGenerator;
+    ShapeGenerator shapeGenerator;
     Mesh mesh;
     int resolution;
     Vector3 localUp;
@@ -13,7 +13,7 @@ public class TerrainFace {
 
     public TerrainFace(ShapeGenerator shapeGenerator, Mesh mesh, int resolution, Vector3 localUp)
     {
-        this.ShapeGenerator = shapeGenerator;
+        this.shapeGenerator = shapeGenerator;
         this.mesh = mesh;
         this.resolution = resolution;
         this.localUp = localUp;
@@ -36,7 +36,7 @@ public class TerrainFace {
                 Vector2 percent = new Vector2(x, y) / (resolution - 1);
                 Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
                 Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
-                vertices[i] = pointOnUnitSphere;
+                vertices[i] = shapeGenerator.CalculatePoinOnPlanet(pointOnUnitSphere);
 
                 if (x != resolution - 1 && y != resolution - 1)
                 {
