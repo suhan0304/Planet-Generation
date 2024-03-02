c62e558eb164b7e2e390c613dbce9082e0a8ff51
ZerrY <suhan0304@gmail.com>
2024-03-03 02:54:25 +0900

    Planet Generation use noiseFilters

--- PlanetGeneration/Assets/Scripts/ShapeGenerator.cs
+++ PlanetGeneration/Assets/Scripts/ShapeGenerator.cs
@@ -19,11 +19,24 @@ public class ShapeGenerator
 
     public Vector3 CalculatePoinOnPlanet(Vector3 pointOnUnitSphere)
     {
+        float firstLayerValue = 0;
         float elevation = 0;
-        for (int i = 0; i < noiseFilters.Length; i++)
+
+        if (noiseFilters.Length > 0)
+        {
+            firstLayerValue = noiseFilters[0].Evalute(pointOnUnitSphere);
+            if (settings.noiseLayers[0].enabled)
+            {
+                elevation = firstLayerValue;
+            }
+        }
+        for (int i = 1; i < noiseFilters.Length; i++)
         {
             if (settings.noiseLayers[i].enabled)
-                elevation += noiseFilters[i].Evalute(pointOnUnitSphere);
+            {
+                float mask = (settings.noiseLayers[i].useFirstLayerAsMask) ? firstLayerValue : 1;
+                elevation += noiseFilters[i].Evalute(pointOnUnitSphere) * mask;
+            }
         }
         return pointOnUnitSphere * settings.planetRadius * (1 + elevation); ;
     }
