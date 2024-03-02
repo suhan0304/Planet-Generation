4351efa4d030d387f7feec126f2f292c2b1c8560
ZerrY <suhan0304@gmail.com>
2024-03-03 02:12:39 +0900

    Update NoiseFilter.cs

--- PlanetGeneration/Assets/Scripts/NoiseFilter.cs
+++ PlanetGeneration/Assets/Scripts/NoiseFilter.cs
@@ -1,14 +1,21 @@
+using Palmmedia.ReportGenerator.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NoiseFilter
 {
+    NoiseSettings settings;
     Noise noise = new Noise();
+
+    public NoiseFilter(NoiseSettings settings) 
+    { 
+        this.settings = settings; 
+    }
 
     public float Evalute(Vector3 point)
     {
-        float noiseValue = noise.Evaluate(point);
+        float noiseValue = (noise.Evaluate(point));
         return noiseValue;
     }
 }
