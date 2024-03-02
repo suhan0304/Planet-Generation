6508e1db5017893f982a4a1903c67fc4b8f843fe
ZerrY <suhan0304@gmail.com>
2024-03-03 02:08:37 +0900

    Update ShapeGenerator.cs

--- PlanetGeneration/Assets/Scripts/ShapeGenerator.cs
+++ PlanetGeneration/Assets/Scripts/ShapeGenerator.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class ShapeGenerator 
 {
     ShapeSettings settings;
-
+    NoiseFilter noiseFilter;
     public ShapeGenerator(ShapeSettings settings)
     {
         this.settings = settings;
+        noiseFilter = new NoiseFilter();
     }
 
     public Vector3 CalculatePoinOnPlanet(Vector3 pointOnUnitSphere)
     {
+        float elevation = noiseFilter.Evalute(pointOnUnitSphere);
         return pointOnUnitSphere * settings.planetRadius;
     }
 }
