b780a9f544b6f5d7ab75d3e04d572a3e6ed45a8a
ZerrY <suhan0304@gmail.com>
2024-03-03 02:23:30 +0900

    Update NoiseSettings.cs

--- PlanetGeneration/Assets/Scripts/NoiseSettings.cs
+++ PlanetGeneration/Assets/Scripts/NoiseSettings.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class NoiseSettings 
 {
     public float strength = 1;
+    [Range(1, 8)]
+    public int numLayers = 1;
+    public float baseRoughness;
     public float roughness = 1;
+    public float persistence = .5f;
     public Vector3 centre;
 }
