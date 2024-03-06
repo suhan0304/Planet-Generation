14f791f35c4517c67baf273a0a212d32bb74c8c9
ZerrY <suhan0304@gmail.com>
2024-03-07 00:18:05 +0900

    Update RigidNoiseFilter.cs

--- PlanetGeneration/Assets/Scripts/RigidNoiseFilter.cs
+++ PlanetGeneration/Assets/Scripts/RigidNoiseFilter.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RigidNoiseFilter : MonoBehaviour
+public class RigidNoiseFilter
 {
-    // Start is called before the first frame update
-    void Start()
+    NoiseSettings settings;
+    Noise noise = new Noise();
+
+    public RigidNoiseFilter(NoiseSettings settings)
     {
-        
+        this.settings = settings;
     }
 
-    // Update is called once per frame
-    void Update()
+    public float Evalute(Vector3 point)
     {
-        
+        float noiseValue = 0;
+        float frequency = settings.baseRoughness;
+        float amplitude = 1;
+
+        for (int i = 0; i < settings.numLayers; i++)
+        {
+            float v = noise.Evaluate(point * frequency + settings.centre);
+            noiseValue += (v + 1) * .5f * amplitude;
+            frequency *= settings.roughness;
+            amplitude *= settings.persistence;
+        }
+
+        noiseValue = Mathf.Max(0, noiseValue - settings.minValue);
+        return noiseValue * settings.strength;
     }
 }
