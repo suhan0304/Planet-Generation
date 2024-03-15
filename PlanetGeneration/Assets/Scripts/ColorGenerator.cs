4ddb95c4cd147a9961ba7855fe9f2afa5228fd72
ZerrY <suhan0304@gmail.com>
2024-03-16 00:44:46 +0900

    Update ColorGenerator.cs

--- PlanetGeneration/Assets/Scripts/ColorGenerator.cs
+++ PlanetGeneration/Assets/Scripts/ColorGenerator.cs
@@ -34,6 +34,10 @@ public class ColorGenerator
             {
                 biomeIndex = i;
             }
+            else
+            {
+                break;
+            }
         }
     }
 
