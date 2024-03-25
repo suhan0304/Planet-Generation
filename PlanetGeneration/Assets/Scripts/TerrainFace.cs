3e5e1cdfa85c66b29311b2cab17e8ad25f3f6009
ZerrY <suhan0304@gmail.com>
2024-03-26 00:39:23 +0900

    Update TerrainFace.cs

--- PlanetGeneration/Assets/Scripts/TerrainFace.cs
+++ PlanetGeneration/Assets/Scripts/TerrainFace.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class TerrainFace {
@@ -37,7 +38,9 @@ public class TerrainFace {
                 Vector2 percent = new Vector2(x, y) / (resolution - 1);
                 Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
                 Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
-                vertices[i] = shapeGenerator.CalculatePoinOnPlanet(pointOnUnitSphere);
+                float unscaledElevation = shapeGenerator.CalculateUnscaledElevation(pointOnUnitSphere);
+                vertices[i] = pointOnUnitSphere * shapeGenerator.GetScaledElevation(unscaledElevation);
+                uv[i].y = unscaledElevation;
 
                 if (x != resolution - 1 && y != resolution - 1)
                 {
