79dca13397572ee81b5d5aac09ffcb2b902a2011
ZerrY <suhan0304@gmail.com>
2024-03-26 00:34:50 +0900

    Update ShapeGenerator.cs

--- PlanetGeneration/Assets/Scripts/ShapeGenerator.cs
+++ PlanetGeneration/Assets/Scripts/ShapeGenerator.cs
@@ -19,7 +19,7 @@ public class ShapeGenerator
         elevationMinMax = new MinMax();
     }
 
-    public Vector3 CalculatePoinOnPlanet(Vector3 pointOnUnitSphere)
+    public float CalculateUnscaledElevation(Vector3 pointOnUnitSphere)
     {
         float firstLayerValue = 0;
         float elevation = 0;
@@ -40,8 +40,14 @@ public class ShapeGenerator
                 elevation += noiseFilters[i].Evaluate(pointOnUnitSphere) * mask;
             }
         }
-        elevation = settings.planetRadius * (1 + elevation);
         elevationMinMax.AddValue(elevation);
-        return pointOnUnitSphere * elevation;
+        return elevation;
+    }
+
+    public float GetScaledElevation(float unscaledElevation)
+    {
+        float elevation = Mathf.Max(0, unscaledElevation);
+        elevation = settings.planetRadius * (1 + elevation);
+        return elevation;
     }
 }
